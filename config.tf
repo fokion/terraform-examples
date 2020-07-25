bbacd2046cad28af761a721c366bdff06b54c02f
Fokion Sotiropoulos <fokion.s@gmail.com>
2020-07-25 17:44:32 +0100

    remote state

--- config.tf
+++ config.tf
@@ -2,3 +2,11 @@ provider "aws"{
     profile = "default"
     region  = "eu-west-1"
 }
+terraform {
+    backend "s3" {
+        bucket = "tf-planner"
+        key = "terraform/state"
+        region = "eu-west-1"
+        dynamodb_table = "tf-planner-state"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "application" {
+  type = string
+  description = "Application"
+}
+variable "root_domain" {
+  type = string
+  description = "Root domain"
+}
+
+variable "application-image" {
+  type = string
+  description = "The docker image"
+}
