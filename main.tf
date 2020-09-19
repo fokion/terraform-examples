8e6c1c72ea7fe00899bbbbbc822906a051e2590d
Fokion Sotiropoulos <fokion.s@gmail.com>
2020-09-19 23:10:45 +0100

    fixup

--- main.tf
+++ main.tf
@@ -1,87 +1,52 @@
-resource "aws_default_vpc" "default" {
-  instance_tenacy = "default"
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "Default VPC"
-  }
-}
-resource "aws_subnet" "eu-1a" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = aws_default_vpc.default.id
-  availability_zone = "eu-west-1a"
-}
-resource "aws_subnet" "eu-1b" {
-  cidr_block = "10.0.2.0/24"
-  vpc_id = aws_default_vpc.default.id
-  availability_zone = "eu-west-1b"
-}
-
-resource "aws_subnet" "eu-private-1a" {
-  cidr_block = "10.0.3.0/24"
-  vpc_id = aws_default_vpc.default.id
-  map_public_ip_on_launch = false
-  availability_zone = "eu-west-1a"
-}
-resource "aws_subnet" "eu-private-1b" {
-  cidr_block = "10.0.4.0/24"
-  vpc_id = aws_default_vpc.default.id
-  map_public_ip_on_launch = false
-  availability_zone = "eu-west-1b"
-}
-
-resource "aws_internet_gateway" "internet-gateway" {
-  vpc_id = aws_default_vpc.default.id
-  tags = {
-    Name = "Main Gateway"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
   }
 }
 
 
-resource "aws_route_table" "main-routetable" {
-  vpc_id = aws_default_vpc.default.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet-gateway.id
+variable "env_vars" {
+   type = map(any)
+}
+resource "aws_lambda_function" "example" {
+  function_name = "ServerlessExample"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "fokion-lambda-tests"
+  s3_key    = "v1.0.0/example.zip"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "server.handler"
+  runtime = "nodejs12.x"
+  environment {
+    variables = merge(var.env_vars,{APP_SECRET=uuid()})
   }
-  tags = {
-    Name = "main-public"
-  }
+  role = aws_iam_role.lambda_exec.arn
 }
 
-resource "aws_route_table_association" "eu_public-1a" {
-  route_table_id = aws_route_table.main-routetable.id
-  subnet_id = aws_subnet.eu-1a.id
-}
-resource "aws_route_table_association" "eu_public-1b" {
-  route_table_id = aws_route_table.main-routetable.id
-  subnet_id = aws_subnet.eu-1b.id
-}
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
 
-
-
-resource "aws_security_group" "application-sg"{
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_default_vpc.default.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [aws_subnet.eu-1a.cidr_block,aws_subnet.eu-1b.cidr_block]
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        Action: "sts:AssumeRole",
+        Principal: {
+          "Service": "lambda.amazonaws.com"
+        },
+        Effect: "Allow",
+        Sid: ""
+      }
+    ]
   }
+  )
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
 }
+
