6643067a6499e252e076f85e1fdcec0b2ec9a931
Fokion Sotiropoulos <fokion.s@gmail.com>
2020-09-20 00:40:30 +0100

    switch to http

--- main.tf
+++ main.tf
@@ -46,69 +46,32 @@ resource "aws_iam_role" "lambda_exec" {
 }
 
 
-resource "aws_api_gateway_rest_api" "api_gateway" {
+resource "aws_apigatewayv2_api" "api_gateway" {
   name        = "ServerlessExample"
   description = "Terraform Serverless Application Example"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "{proxy+}"
+
+resource "aws_apigatewayv2_route" "example" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "$default"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_app.invoke_arn
-}
-
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy_root.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_app.invoke_arn
-}
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_app.function_name
   principal     = "apigateway.amazonaws.com"
 
-  # The "/*/*" portion grants access from any method on any resource
-  # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+  source_arn = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
 }
-resource "aws_api_gateway_deployment" "live_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.lambda,
-    aws_api_gateway_integration.lambda_root,
-  ]
+resource "aws_apigatewayv2_deployment" "example" {
+  api_id      = aws_apigatewayv2_route.example.api_id
+  description = "Example deployment"
 
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = "live"
-}
-
-output "base_url" {
-  value = aws_api_gateway_deployment.live_deployment.invoke_url
+  lifecycle {
+    create_before_destroy = true
+  }
 }
