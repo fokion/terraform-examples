00b1aaa662a992e10e5ee1b21d151d3dd46d63e0
Fokion Sotiropoulos <fokion.s@gmail.com>
2021-01-24 12:04:08 +0000

    update hosted zone

--- main.tf
+++ main.tf
@@ -8,60 +8,6 @@ terraform {
 
 
 
-
-resource "aws_lambda_function" "lambda_app" {
-  function_name = var.lambda_function_name
-  s3_bucket = var.lambda_s3_bucket
-  s3_key = var.lambda_s3_key
-  handler = var.lambda_handler
-  timeout = 30
-  description = "${var.lambda_function_name} λ"
-  runtime = "nodejs12.x"
-  environment {
-    variables = merge(var.env_vars,{APP_SECRET=uuid()})
-  }
-  tags = var.tags
-  role = aws_iam_role.lambda_exec.arn
-}
-
-# IAM role which dictates what other AWS services the Lambda function
-# may access.
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_example_lambda"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        Action: "sts:AssumeRole",
-        Principal: {
-          "Service": "lambda.amazonaws.com"
-        },
-        Effect: "Allow",
-        Sid: ""
-      }
-    ]
-  }
-  )
-
-}
-resource "random_id" "server" {
-  byte_length = 8
-}
-
-resource "aws_apigatewayv2_api" "api_gateway" {
-  name        = "ServerlessExample-${random_id.server.hex}"
-  description = "Terraform Serverless Application Example"
-  protocol_type = "HTTP"
-  target        = aws_lambda_function.lambda_app.arn
-}
-
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_app.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+resource "aws_route53_zone" "main" {
+  name = "fokion.xyz"
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-variable "lambda_function_name" {
-  type = string
-  default = "ServerlessApplication"
-}
-
-variable "lambda_s3_bucket" {
-  type = string
-  default = "fokion-lambda-tests"
-}
-
-variable "lambda_s3_key" {
-  type = string
-  default = "server.zip"
-}
-
-variable "lambda_handler" {
-  type = string
-  default = "server.handler"
-}
-
 variable "tags" {
   type = map(any)
   default = {}
